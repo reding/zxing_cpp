18632802242e496038525ecf0f64b685c8be6e69
guojianpeng <guojianpeng@meituan.com>
2017-03-27 17:29:34 +0800

    init

---
+++ app/src/main/java/com/reding/zxingcpp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.reding.zxingcpp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText("hello world");
+    }
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("zxing");
+    }
+}
